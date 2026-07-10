2dc263977b70f2a63cc057c210527736eb05b51b
Nasaran-Ruchipornnithipat <nasaran.r@ku.th>
2026-07-10 15:10:04 +0700

    week 2 arithmetic-operators

---
+++ labs/week02-lab/example/test-01.py
@@ -0,0 +1,18 @@
+print("Now try these exercises:")
+print()
+print("1. Circle Calculator:")
+print("   - Ask user for radius")
+print("   - Calculate area (π * r²)")
+print("   - Calculate circumference (2 * π * r)")
+print("   - Use 3.14159 for π")
+print()
+
+radius = float(input("Enter Rectangla radius:"))
+
+area = (3.14159 * radius**radius)
+circumference = (2 * 3.14159 * radius)
+
+print("Area:",area)
+print("circimference" + str(circumference))
+
+
---
+++ labs/week02-lab/example/test-02.py
@@ -0,0 +1,14 @@
+print("4. BMI Calculator:")
+print("   - Ask for weight (kg) and height (m)")
+print("   - Calculate: BMI = weight / (height ** 2)")
+print()
+
+
+weight = str(input("Enter your weight"))
+height = str(input("Enter your height"))
+
+bmi =  weight / (height ** 2)
+
+print("bmi:")
+
+
