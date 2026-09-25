492d04be602b29938fe184e8ed885150a8145df1
Nasaran-Ruchipornnithipat <nasaran.r@kku.th>
2026-09-25 23:37:33 +0700

    OOP1

--- labs/week07-lab/example/Car.py
+++ labs/week07-lab/example/Car.py
@@ -19,14 +19,14 @@ class Car:
         """Method to get car information"""
         return f"{self.year} {self.brand} {self.model} - Mileage: {self.mileage} km"
     
-    @classmethod
+    @classmethod# method 
     def get_vehicle_type(cls):
         """Class method to access class attributes"""
         return cls.vehicle_type
 
-# Creating instances
-car1 = Car("Toyota", "Camry", 2022)
-car2 = Car("Honda", "Civic", 2021)
+# Creating instances การสร้างวัตถุจากคลาส
+car1 = Car("Toyota", "Camry", 2022) # mileage = 0
+car2 = Car("Honda", "Civic", 2021) # mileage = 0
 
 # Accessing class attributes
 print(f"All cars have {Car.wheels} wheels")
--- labs/week07-lab/example/class-draft.py
+++ labs/week07-lab/example/class-draft.py
@@ -1,15 +1,19 @@
 class ClassName:
     """Class docstring"""
+
     
+    # ข้อมูล ที่จำเป็นในการแก้ปัญหา template/แม่แบบ/พิมพิ์เขียว/ตรายาง
     def __init__(self, parameters):
         # Constructor method
-        self.attribute = value
-    
+        self.attribute2 = value
+        self.attribute3 = value
+
     def method_name(self):
         # Instance method
         return something
 
-
+    def method_name2(self):
+        return ...
 myObj = ClassName(parameters)
 print(myObj.attribute)
 resultFromMethod = myObj.method_name()
